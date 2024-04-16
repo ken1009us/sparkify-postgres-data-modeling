a8262e567331aa8d79245b98b99ad7b2418f448e
Ken <ken.wu@dimaresaas.com>
2024-04-16 15:22:32 -0700

    add artist data func

--- etl.py
+++ etl.py
@@ -53,3 +53,61 @@ def extract_song_data(songs_df):
         print(f"Error: {e}")
         if conn:
             conn.rollback()
+
+
+def extract_artist_data(songs_df):
+    selected_columns = ['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']
+    artist_df = songs_df[selected_columns]
+
+    if not artist_df.empty:
+        artist_data = artist_df.values.tolist()
+    else:
+        artist_data = []
+
+    artist_table_insert = ("INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES(%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING")
+
+    try:
+        cur = conn.cursor()
+        for artist in artist_data:
+            cur.execute(artist_table_insert, artist)
+        conn.commit()
+        cur.close()
+    except Exception as e:
+        print(f"Error: {e}")
+        if conn:
+            conn.rollback()
+
+
+def extract_time_data(log_files_df):
+    log_df = log_files_df[log_files_df['page'] == 'NextSong']
+    log_df['ts'] = pd.to_datetime(log_df['ts'], unit='ms')
+
+    time_data = {
+        "start_time": log_df['ts'],
+        "hour": log_df['ts'].dt.hour,
+        "day": log_df['ts'].dt.day,
+        "week": log_df['ts'].dt.isocalendar().week,
+        "month": log_df['ts'].dt.month,
+        "year": log_df['ts'].dt.year,
+        "weekday": log_df['ts'].dt.weekday
+    }
+
+    column_labels = ["start_time", "hour", "day", "week", "month", "year", "weekday"]
+    time_df = pd.DataFrame(time_data, columns=column_labels)
+
+    print(time_df.head())
+
+
+def main():
+    # data_dir = "data/song_data"
+    # songs_df = get_files(data_dir)
+    # extract_song_data(songs_df)
+    # extract_artist_data(songs_df)
+    log_data_dir = "data/log_data"
+    log_files_df = get_files(log_data_dir)
+    extract_time_data(log_files_df)
+
+
+
+if __name__ == "__main__":
+    main()
